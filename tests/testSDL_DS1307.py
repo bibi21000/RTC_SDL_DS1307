5cc3359a752d04b3026cb7e4cb0cfabe1ad42300
bibi21000 <bibi21000@gmail.com>
2017-12-14 19:48:07 +0100

    Update tests

--- tests/testSDL_DS1307.py
+++ tests/testSDL_DS1307.py
@@ -47,7 +47,7 @@ print "Program Started at:"+ time.strftime("%Y-%m-%d %H:%M:%S")
 filename = time.strftime("%Y-%m-%d%H:%M:%SRTCTest") + ".txt"
 starttime = datetime.datetime.utcnow()
 
-ds1307 = SDL_DS1307(1, 0x68)
+ds1307 = SDL_DS1307(address=0x68)
 ds1307.write_now()
 
 # Main Loop - sleeps 10 minutes, then reads and prints values of all clocks
